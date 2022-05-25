963d4b2d1f2451c9f36c8b739e6ebe6f7d736f5b
prakramrathore <76489336+prakramrathore@users.noreply.github.com>
2022-05-25 10:42:52 +0530

    First commit

---
+++ lib/demos/cupertino/demo_types.dart
@@ -0,0 +1,9 @@
+// @dart=2.9
+
+enum AlertDemoType {
+  alert,
+  alertTitle,
+  alertButtons,
+  alertButtonsOnly,
+  actionSheet,
+}
---
+++ tool/l10n_cli/main.dart
@@ -0,0 +1,11 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart=2.9
+
+import 'l10n_cli.dart';
+
+void main(List<String> arguments) {
+  arbToXml();
+}
